0952da9315e32713102021ff3c01dc58820e68ff
aleksandr.developer.web <aleksandr.developer.web@gmail.com>
2023-06-29 20:13:02 +0300

    commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e20947e95d8b36131884adb3fb17a097",
-"index.html": "b63f2a62efda69244922291ccc20cbfa",
-"/": "b63f2a62efda69244922291ccc20cbfa",
-"main.dart.js": "74a23c225d0cfd8da9a5fcdea640a0d1",
+"index.html": "74c92e251c921b37c3c8352293a1b23f",
+"/": "74c92e251c921b37c3c8352293a1b23f",
+"main.dart.js": "c01dbf7c1b41923f590070a3b30133cd",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
