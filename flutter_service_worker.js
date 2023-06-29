f9be87a401ddead0ea78512c06b1ee68ecf44a37
Aleksandr Omeluanenko <106429142+AleksandrDeveloper@users.noreply.github.com>
2023-06-29 22:11:05 +0300

    test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e20947e95d8b36131884adb3fb17a097",
-"index.html": "3d41f9a78fe058398b9f3f5e82ffa8a8",
-"/": "3d41f9a78fe058398b9f3f5e82ffa8a8",
+"index.html": "2e4293c188b215778bec0b024e26c1bf",
+"/": "2e4293c188b215778bec0b024e26c1bf",
 "main.dart.js": "52d93593c8858dbb30a6424a7e029746",
+"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -31,10 +32,10 @@ const RESOURCES = {
 "assets/assets/svg/calendar.svg": "7e46ca1d0952158b7b5eeabd59106db3",
 "assets/assets/svg/splash_screen_app_bar.svg": "ef7268cc3a78ce4d27fb83065113e5e8",
 "assets/assets/png/splash_screen_image.png": "6bacffab80bfd51dd295145a6f4b56a4",
-".idea/workspace.xml": "5e0dfd199decdafa78f7c52334468608",
-".idea/modules.xml": "1eb3b065c55895e347308e50cd717dee",
-".idea/web.iml": "52db5efd0fe9f576a1302b8c4b5eac6c",
-".idea/misc.xml": "e6b023077aa4876dbe12734108325458"
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
 };
 
 // The application shell files that are downloaded before a service worker can
