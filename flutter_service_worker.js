41055d838e5ecdd2108e918f22b18984d7deac0c
Aleksandr Omeluanenko <106429142+AleksandrDeveloper@users.noreply.github.com>
2023-07-03 09:32:22 +0300

    test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e20947e95d8b36131884adb3fb17a097",
-"index.html": "1021c38df7ae27fc55f318bcb52300a3",
-"/": "1021c38df7ae27fc55f318bcb52300a3",
-"main.dart.js": "11b3963c86052311fa8c25461a1df7c6",
+"index.html": "2f7bba2cb6552d5c67f1537ea8ec83bb",
+"/": "2f7bba2cb6552d5c67f1537ea8ec83bb",
+"main.dart.js": "b28edabf00e1184d40c96f174168f04d",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
